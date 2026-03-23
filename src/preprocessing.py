9f836df0628d6c45b85f6a1c03fdf39218cd0b37
aayaou-wq <a.ayaou@ump.ac.ma>
2026-03-23 14:09:01 +0100

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -13,10 +13,8 @@ def add_rul_to_train(train_df: pd.DataFrame) -> pd.DataFrame:
 
 def add_rul_to_test(test_df: pd.DataFrame, rul_df: pd.DataFrame) -> pd.DataFrame:
     df = test_df.copy()
-
     max_cycles = df.groupby("unit_nr")["time_cycles"].max().reset_index()
     max_cycles.columns = ["unit_nr", "max_cycle"]
-
     df = df.merge(max_cycles, on="unit_nr", how="left")
     df = df.merge(rul_df, on="unit_nr", how="left")
     df["RUL"] = (df["max_cycle"] - df["time_cycles"]) + df["RUL"]
